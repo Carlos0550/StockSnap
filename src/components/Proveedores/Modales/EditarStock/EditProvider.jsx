055c5d6460756e2c0fdfabe323654f49f20ecb8f
Carlos0550 <carlospelinski05@gmail.com>
2024-08-12 23:59:13 -0300

    modal para editar stock

---
+++ src/components/Proveedores/Modales/EditarStock/EditProvider.jsx
@@ -0,0 +1,24 @@
+import { Button, Flex, Modal } from 'antd'
+import React from 'react'
+import AddProviders from '../../AñadirProveedor/AddProviders'
+function EditProvider({ closeModal,selectedProvider }) {
+  return (
+    <>
+      <Modal
+        open={true}
+        title="Editar proveedor"
+        onCancel={closeModal}
+        footer={[
+          <Button type='primary' danger onClick={closeModal}>Cancelar</Button>,
+        ]}
+      >
+        <Flex vertical >
+          <AddProviders editingProvider={true} selectedProvider={selectedProvider} closeModalEditProvider={closeModal}/>
+        </Flex>
+
+      </Modal>
+    </>
+  )
+}
+
+export default EditProvider
