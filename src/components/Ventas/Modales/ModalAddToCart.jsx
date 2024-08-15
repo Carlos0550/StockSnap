3b2998f6be1fb8a778a566a3fb50c469fda94603
Carlos0550 <carlospelinski05@gmail.com>
2024-08-15 11:10:49 -0300

    Modal para añadir cantidad a los productos

--- src/components/Ventas/Modales/ModalAddToCart.jsx
+++ src/components/Ventas/Modales/ModalAddToCart.jsx
@@ -1,15 +1,41 @@
-import { Button, Modal } from 'antd'
-import React, { useState } from 'react'
+import { Button, message, Modal, Tag } from 'antd'
+import React, { useEffect, useState } from 'react'
 import { Card, Col, Row, Flex, InputNumber } from 'antd';
 import { TextField } from '@mui/material';
-
+import { useAppContext } from '../../../utils/contexto';
+import { AddShoppingCartSharp } from '@mui/icons-material';
+const { Meta } = Card
 function ModalAddToCart({ closeModal, selectedProduct }) {
     const [quantityValue, setQuantityValue] = useState(1)
-    console.log(quantityValue)
+    const { setCart, cart } = useAppContext()
     const onChange = (value) => {
         setQuantityValue(value);
 
     };
+
+   const handleAddToCart = () =>{
+        const prevCart = [...cart]
+        const id_itemToAdd = selectedProduct.id_producto
+        const existingItem = prevCart.find(item => item.id_producto === id_itemToAdd);
+
+       if (existingItem) {
+            existingItem.quantity = quantityValue
+       }else{
+        prevCart.push({
+            ...selectedProduct,
+            quantity: quantityValue
+        })
+       }
+
+       setCart(prevCart)
+       message.success(`${selectedProduct.nombre_producto} añadido!`)
+       closeModal()
+   }
+
+
+    // useEffect(()=>{
+    //     console.log("Carrito serializado ", JSON.stringify(cart))
+    // },[cart])
     return (
         <>
             <Modal
@@ -19,24 +45,40 @@ function ModalAddToCart({ closeModal, selectedProduct }) {
                     <Button type='primary' danger onClick={closeModal}>Cerrar</Button>
                 ]}
             >
-                <Flex>
-                    <TextField
-                        id={selectedProduct.id_producto}
-                        value={selectedProduct.nombre_producto}
-                        label="Nombre del producto"
-                        size='small'
-                        InputProps={{ readOnly: true }}
-                    />
+                <Flex vertical>
+                    <Card hoverable style={{ margin: "1rem" }}>
+                        <Meta title={selectedProduct.nombre_producto} description={(
+                            <>
+                                <Tag
+                                    color={quantityValue > selectedProduct.stock ? 'red' : 'green'}
+                                    style={{ fontSize: "1rem", marginTop: "1rem", marginBottom: "1rem" }}
+                                >
+                                    {quantityValue > selectedProduct.stock ? "La cantidad supera el stock disponible" : (
+                                            <>
+                                                Ingrese una cantidad no mayor a: <strong>{selectedProduct.stock}</strong>
+                                            </>
+                                        )}
+                                </Tag>  
+                                <Flex gap="small">
+                                <Col span={12}>
+                                    <InputNumber
+                                        min={1}
+                                        value={quantityValue}
+                                        onChange={onChange}
+                                        style={{ width: '100%' }}
+                                        status={quantityValue > selectedProduct.stock ? "error" : ""}
+                                    />
+                                </Col>
+                                <Button type='primary' onClick={handleAddToCart}>Añadir al carrito <AddShoppingCartSharp/></Button>
+                                </Flex>
+                            </>
+                        )} />
 
-                    <Col span={12}>
-                        <InputNumber
-                            min={1}
-                            value={quantityValue}
-                            onChange={onChange}
-                            style={{ width: '100%' }}
-                            status={quantityValue > selectedProduct.stock ? "error" : ""}
-                        />
-                    </Col>
+                    </Card>
+
+
+
+
                 </Flex>
             </Modal>
         </>
