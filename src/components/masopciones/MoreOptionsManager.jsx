abdc6e11ca3d05be3a863bd414f2eaf44b636e52
Carlos0550 <carlospelinski05@gmail.com>
2024-08-19 23:59:31 -0300

    añadido el componente ver clientes

--- src/components/masopciones/MoreOptionsManager.jsx
+++ src/components/masopciones/MoreOptionsManager.jsx
@@ -2,16 +2,22 @@ import { Collapse } from 'antd'
 import React, { useEffect, useState } from 'react'
 import ShowDIskInfo from './MostrarInfoDisco/ShowDIskInfo'
 import SalesHistoryModal from './Modales/SalesHistoryModal'
+import CreateClients from './AdministrarClientes/CrearClientes/CreateClients'
+import ManageClients from './AdministrarClientes/AdministrarClientes/ManageClients'
 
 function MoreOptionsManager() {
     const [activeKey, setActiveKey] = useState(null)
     const [activeModalSalesHistory, setActiveModalSalesHistory] = useState(false)
-
+    const [activeModalListClients, setActiveModalListClients] = useState(false);
 
     useEffect(()=>{
         if (activeKey && activeKey[0] === "2") {
             setActiveModalSalesHistory(true)
             setActiveKey(null)        
+        }
+        if (activeKey && activeKey[0] === "4") {
+            setActiveModalListClients(true)
+            setActiveKey(null)
         }
     },[activeKey])
     const items = [
@@ -24,12 +30,23 @@ function MoreOptionsManager() {
             key: "2",
             label:"Revisar historial de ventas",
             children:null
+        },
+        {
+            key: "3",
+            label:"Crear un cliente",
+            children: <CreateClients/>
+        },
+        {
+            key: "4",
+            label:"Gestionar Clientes",
+            children: null
         }
     ]
   return (
     <>
         <Collapse accordion items={items} activeKey={activeKey} onChange={((value)=> setActiveKey(value))}/>
         {activeModalSalesHistory && <SalesHistoryModal closeModal={()=> setActiveModalSalesHistory(false)}/>}
+        {activeModalListClients && <ManageClients closeModal={()=> setActiveModalListClients(false)}/>}
     </>
   )
 }
