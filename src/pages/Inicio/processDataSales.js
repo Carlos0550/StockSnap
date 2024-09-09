00efdb49f1c9f7d14d8ea46dcfe815e2125d6a42
Loscar01 <carlospelinski03@gmail.com>
2024-09-09 01:14:51 -0300

    defincion de columnas y procesamiento de datos para las ventas

--- src/pages/Inicio/processDataSales.js
+++ src/pages/Inicio/processDataSales.js
@@ -1,9 +1,44 @@
+import { Button } from "antd"
+import { ShoppingCartOutlined } from "@ant-design/icons"
 
 export const processDataSales = (vistaVentas) =>{
+    console.log(vistaVentas)
     return vistaVentas.map((item,index)=>({
         idProducto: item.id_producto,
+        nombre: item.nombre_producto,
         categoria: item.nombre_categoria,
         precio: item.precio_unitario,
         stock: item.stock
     }))
 }
+
+export const columnsTableSales = [
+    {
+        title: "Producto",
+        key: "producto",
+        dataIndex: "nombre"
+    },
+    {
+        title: "Precio",
+        key: "precio",
+        dataIndex: "precio",
+        render: (_,record) => (
+            parseFloat(record.precio).toLocaleString("es-ES",{style: "currency", currency: "ARS"})
+        )
+    },
+    {
+        title: "Stock",
+        key: "stock",
+        dataIndex: "stock"
+    },
+    {
+        title: "",
+        key: "",
+        render: (_,record) => (
+            <>
+                <Button><ShoppingCartOutlined/></Button>
+            </>
+        )
+    },
+    
+]
